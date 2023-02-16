039b20dee631e6308df7217450848af1ec58b732
KENC0DE <kenansameseret@gmail.com>
2023-02-16 23:18:55 +0000

    Commiting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Prints "Programming is like building a multilingual puzzle"
+ *
+ * Description: The main function is the entry point of the program. It
+ * prints the string "Programming is like building a multilingual puzzle"
+ * to the console using the printf function. The string is followed by a
+ * newline character to ensure that it is printed on a new line. The main
+ * function then returns 0 to indicate successful program completion.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
