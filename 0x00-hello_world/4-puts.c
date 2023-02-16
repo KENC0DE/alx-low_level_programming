f28c8f57282f55b9310a92391ea7f1dc0f7a9022
KENC0DE <kenansameseret@gmail.com>
2023-02-16 23:54:11 +0000

    Commiting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -13,8 +13,7 @@
  */
 int main(void)
 {
-char str = "\"Programming is like building a multilingual puzzle";
-puts(str\n);
+puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
 }
