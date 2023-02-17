6dce125552d629921aae70d6a3568f9cd5b6a920
KENC0DE <kenansameseret@gmail.com>
2023-02-17 01:43:48 +0000

    Commiting

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints error to standart error out put
+ *
+ * Description: on the addvanced challenges
+ *
+ * Return: 1 (error printed)
+ */
+int main(void)
+{
+fpirntf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
