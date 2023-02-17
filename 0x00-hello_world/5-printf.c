2ebbb567e1247f7b3e8a87b622f659e6a8c50f20
KENC0DE <kenansameseret@gmail.com>
2023-02-17 00:09:21 +0000

    Commiting

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,
+ *
+ * Description - a challeng given to me by ALX-africa.
+ * I am dealing with it right now
+ *
+ * return - 0 (succeed)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
