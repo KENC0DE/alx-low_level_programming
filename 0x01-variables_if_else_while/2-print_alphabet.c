c3c5678ab1fd5f3fae502ff87bada9a851a34938
KENC0DE <kenansameseret@gmail.com>
2023-02-18 00:07:43 +0000

    commiting

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - My entry point
+ *
+ * Description: alphabets
+ *
+ * Return: success(0)
+ */
+int main(void)
+}
+char letter = 'a';
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+putchar(\n);
+return (0);
+}
