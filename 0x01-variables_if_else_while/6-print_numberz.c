ed202a246b9a54857f2fae85fc2cec9133e741e7
KENC0DE <kenansameseret@gmail.com>
2023-02-18 01:17:42 +0000

    commiting

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - my entry
+ *
+ * Description: nums in chars
+ *
+ * Return: success
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 10; i++)
+{
+putchar((i == 10) ? '1' : i + '0');
+}
+return (0);
+}
