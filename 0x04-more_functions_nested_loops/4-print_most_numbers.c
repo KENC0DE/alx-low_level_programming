a0a8365945311d5b2f6d6604d78cd5c0287117a2
KENC0DE <kenansameseret@gmail.com>
2023-02-24 03:38:00 +0530

    commiting

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,9 @@ void print_most_numbers(void)
 int i;
 	for (i = 48; i <= 57; i++)
 	{
-		if ((i != 50) || (i != 52))
+		if ((i == 50) || (i == 52))
 		{
-			break;
+			continue;
 		}
 		putchar(i);
 	}
