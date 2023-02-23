a7384dd0fa85306342e706acd8609f9f37145acd
KENC0DE <kenansameseret@gmail.com>
2023-02-24 00:47:36 +0530

    commiting

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,23 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
 
-/**
-*main - header file for protoypes
-*void protoypes (void)
-*int protoypes (int)
-*/
+ifndef MAIN_H
+define MAIN_H
 
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-int _putchar(char);
-int _isupper(int);
-int _isdigit(int);
-int mul(int, int);
+nt _isupper(int);
+nt _isdigit(int);
 
-#endif /* MAIN_H */
+#endif
+
