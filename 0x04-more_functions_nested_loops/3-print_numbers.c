510c66ea4fcb4b63707e0747d58f7e5830ac8e07
KENC0DE <kenansameseret@gmail.com>
2023-02-24 02:28:41 +0530

    commiting

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio>
+/**
+ * print_numbers - print numbers though 0 to 9
+ *
+ * Return: no value
+ */
+void print_numbers(void)
+{
+int i;
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+putchar("\n");
+}
