7d327405c02f96639564bd6982679e96758cc1b9
KENC0DE <kenansameseret@gmail.com>
2023-02-23 22:23:20 +0530

    commiting

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,8 @@
+/**
+ * _isupper - chechs if the character is upper case or not
+ * @c: character being checked
+ * Return: is upper(1) if not (0)
+ */
 int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
