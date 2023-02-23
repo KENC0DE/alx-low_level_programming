13ff2189eb5860015821804114f8c0becf2767a6
KENC0DE <kenansameseret@gmail.com>
2023-02-24 03:22:48 +0530

    commiting

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - print numbers except 2 and 4
+ *
+ * Return: 0 if success
+ */
+void print_most_numbers(void)
+{
+int i;
+	for (i = 48; i <= 57; i++)
+	{
+		if ((i != 50) && (i != 52))
+		{
+			i++;
+		}
+		putchar(i);
+	}
+putchar(10);
+}
