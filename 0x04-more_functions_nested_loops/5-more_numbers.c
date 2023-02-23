f3c56d00701019719bdc0ffe45811730430fabbb
KENC0DE <kenansameseret@gmail.com>
2023-02-24 04:48:43 +0530

    commiting

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - print numbes 10 times from 0 to 14
+ *
+ * Return: 0 if success
+ */
+void more_numbers(void)
+{
+	int n, i;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				putchar((i / 10) + '0')
+			}
+			putchar((i % 10) + '0')
+		}
+		putchar(10);
+	}
+}
