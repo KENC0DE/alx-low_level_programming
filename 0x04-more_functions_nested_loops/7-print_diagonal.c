70509aaa1d3acfd8d37de108d3dfadcc057dcc72
KENC0DE <kenansameseret@gmail.com>
2023-02-24 05:51:05 +0530

    commiting

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,13 +13,18 @@ void print_diagonal(int n)
 	{
 		for (int i = 0; i < n; i++)
 		{
-			putchar(32);
+			for (int n = 0; n < i; n++)
+			{
+				putchar(' ');
+			}
+			putchar('\\');
+			if (i <= 0)
+			{
+				continue;
+			}
+			putchar('\n');
 		}
-		putchar(92);
-		putchar('\n');
+
 	}
-	else
-	{
-		putchar('\n');
-	}
+	putchar('\n');
 }
