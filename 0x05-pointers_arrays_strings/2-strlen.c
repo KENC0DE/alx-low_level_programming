d850f8fb9f54e2774a6a601a9acadbdeb847c1a1
KENC0DE <kenansameseret@gmail.com>
2023-02-28 03:48:58 +0530

    commiting

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - return length of string
+ *
+ * @s: the sting
+ *
+ * Return: intiger value
+ */
+ 
+int _strlen(char *s)
+{
+	int ln;
+
+	ln = strlen(*s);
+	return (ln);
+}
