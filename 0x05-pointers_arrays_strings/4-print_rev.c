186fa73d22ce7594a9367fb644f2f7b03a86a6b4
KENC0DE <kenansameseret@gmail.com>
2023-02-28 05:04:41 +0530

    commiting

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,14 +12,12 @@
 
 void print_rev(char *s)
 {
-	int i, j;
-	char temp;
+	int i;
 	int n = strlen(s);
 	
-	for (i = 0, j = n - 1; i < j; i++, j--)
+	for (i = n; i > 0; i--)
 	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
+		putchar(s[i]);
 	}
+	putchar(10);
 }
