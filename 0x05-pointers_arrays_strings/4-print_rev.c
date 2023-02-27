dd244079d73090ac88f051402133e1b20e303126
KENC0DE <kenansameseret@gmail.com>
2023-02-28 04:45:38 +0530

    commiting

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - reverse printing
+ *
+ * @s: target to be reversed
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i, j;
+	char temp;
+	int n = strlen(s);
+	
+	for (i = 0; j = n - 1; i < j; i++; j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
