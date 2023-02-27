6dfd8ea5011d2b2afe3f7133fae1c48962f7b862
KENC0DE <kenansameseret@gmail.com>
2023-02-28 03:17:07 +0530

    commiting

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swap values
+ *
+ * @a: value one
+ * @b: value two
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = 0;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
