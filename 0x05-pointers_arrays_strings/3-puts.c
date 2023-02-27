5eddb42251d50c1904d9fb47aa99d5aa338f0f21
KENC0DE <kenansameseret@gmail.com>
2023-02-28 04:05:18 +0530

    commiting

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - print with new line
+ *
+ * @str: going to be printed
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
