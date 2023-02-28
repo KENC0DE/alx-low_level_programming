1030728476d21a0c200f60edbb17ca40e27f4fde
KENC0DE <kenansameseret@gmail.com>
2023-02-28 07:00:21 +0530

    commiting

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _atoi - number predator
+ *
+ * @s: the jungle
+ *
+ * Return: intiger
+ */
+
+int _atoi(char *s)
+{
+	int numb = atoi(s);
+
+	return (numb);
+}
