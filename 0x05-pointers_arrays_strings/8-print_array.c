fa8d627af82aa0894a434da7684a87a3ad0c0e49
KENC0DE <kenansameseret@gmail.com>
2023-02-28 06:26:30 +0530

    commiting

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_array - print array separated by comma
+ *
+ * @a: pointer
+ * @n: repitition
+ *
+ * Return: nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", *(a + i));
+	}
+}
