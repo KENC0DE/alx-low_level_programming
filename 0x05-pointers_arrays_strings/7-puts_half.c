e5164cbee91d7cad92397a9ae94497ef421df697
KENC0DE <kenansameseret@gmail.com>
2023-02-28 05:47:17 +0530

    commiting

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - print half the string
+ *
+ * @str: to be cut string
+ *
+ * Return: nothig
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int n = strlen(str);
+	int hf;
+
+	if ((n % 2) == 0)
+		hf = n / 2;
+	else
+		hf = (n - 1) / 2;
+
+	for (i = 0; i < n; i++)
+	{
+		if (hf < hf)
+			continue;
+
+		putchar(str[i]);
+	}
+	putchar(10);
+}
