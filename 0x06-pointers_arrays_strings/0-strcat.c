1c92b5a8130956047c91f8e40226af05a27b5ddf
KENC0DE <kenansameseret@gmail.com>
2023-03-02 02:37:22 +0530

    comming

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - concatenate strings
+ *
+ * @dest: first var
+ * @src: second variable
+ *
+ * Return: character
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return(dest);
+}
