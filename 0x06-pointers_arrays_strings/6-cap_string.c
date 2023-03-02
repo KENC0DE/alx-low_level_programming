57b919a85a55e88558f7916e08316c2839cd28d5
KENC0DE <kenansameseret@gmail.com>
2023-03-02 21:18:37 +0530

    commiting

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * cap_string - capitalize bruh
+ *
+ * @str: capitalized
+ *
+ * Return: character
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+	int n = strlen(str);
+
+	for (i = 0; i < n; i++)
+	{
+		if (str[i] == ' ' ||
+			str[i] == ';' ||
+			str[i] == '.' ||
+			str[i] == '!' ||
+			str[i] == '?' ||
+			str[i] == '"' ||
+			str[i] == '(' ||
+			str[i] == ')' ||
+			str[i] == '{' ||
+			str[i] == '}')
+		{
+			str[i + 1] = toupper(str[i + 1]);
+		}
+	}
+	return (str);
+}
