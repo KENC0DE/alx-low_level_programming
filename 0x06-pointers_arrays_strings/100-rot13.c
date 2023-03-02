77f504003b0a9bbb329da53d7c554a556954f700
KENC0DE <kenansameseret@gmail.com>
2023-03-03 02:26:52 +0530

    commiting

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,46 +1,31 @@
 #include "main.h"
 
 /**
- * rot13 - rot13 encoder
- * @str: string to be encoded
- *
- * Return: address of the encoded string
- */
-
-char *rot13(char *str)
+  * rot13 - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
+char *rot13(char *s)
 {
-	int i = 0;
+	int a = 0;
 
-	while (str[i] != '\0')
+	while (s[a])
 	{
-		str[i] = transform_2(str[i]);
-		i++;
-	}
-	return (str);
-}
-
-/**
- * transform_2 - helper function to map a letter with it's rot13 encoding
- * @x: char to be encoded
- *
- * Return: the encoded char
- */
-
-char transform_2(char x)
-{
-	char one[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char two[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	int i = 0;
-	char replacement = x;
-
-	while (i < 52)
-	{
-		if (x == one[i])
+		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
 		{
-			replacement = two[i];
+			if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
+			{
+				s[a] -= 13;
+				break;
+			}
+
+			s[a] += 13;
 			break;
 		}
-		i++;
+
+		a++;
 	}
-	return (replacement);
+
+	return (s);
 }
