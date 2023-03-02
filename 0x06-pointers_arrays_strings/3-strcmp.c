959c9aee80173f46d5b35ad64137ce18eedb7c91
KENC0DE <kenansameseret@gmail.com>
2023-03-02 19:25:25 +0530

    commiting

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - compare
+ *
+ * @s1: compare this
+ * @s2: to this
+ *
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
