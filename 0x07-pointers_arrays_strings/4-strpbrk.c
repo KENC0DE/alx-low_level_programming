4accf7d8f6ab4de59372d70a83a0e851d5f0f50d
KENC0DE <kenansameseret@gmail.com>
2023-03-07 00:42:14 +0530

    commiting

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,43 +1,26 @@
-#include "holberton.h"
-#include <stdio.h>
-
+#include "main.h"
 /**
- * _strpbrk - searches a string for any set of bytes
- * @s: string to be searched
- * @accept: string to be used
+ * _strpbrk -  searches a string for any of a set of bytes.
  *
- * Return: a pointer to the byte in s that matches one of the bytes in accept
+ * @s: char pointer
+ *
+ * @accept: char pointer
+ *
+ * Return: string
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0;
+	unsigned int counter;
 
-	for (; s[i] != '\0' && _strchr(accept, s[i]) == NULL; i++)
-		;
+	for (; *s != '\0'; s++)
+	{
+		for (counter = 0; accept[counter] != '\0'; counter++)
+		{
 
-	if (s[i] == '\0')
-		return (NULL);
-	else
-		return (s + i);
-}
-
-/**
- * _strchr - locates a char in a string
- * @s: string to be searched
- * @c: char to be checked
- *
- * Return: pointer to the first occurence of c in s
- */
-
-char *_strchr(char *s, char c)
-{
-	int i = 0;
-
-	for (; s[i] != c && s[i] != '\0'; i++)
-		;
-	if (s[i] == c)
-		return (s + i);
-	else
-		return (NULL);
+			if (*s == accept[counter])
+				return (s);
+		}
+	}
+	return (0);
 }
