8540872d2968b9bc3f2581d477c0a4e711d87292
KENC0DE <kenansameseret@gmail.com>
2023-03-07 00:55:43 +0530

    commiting

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,23 @@
 #include "main.h"
 /**
- * print_chessboard - print a chessboard
- * @a:array
- * Return:void
+ * print_chessboard - Print a chessboard
+ * @a: Given array holding chessboard
  */
+
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	unsigned int i, j;
 
-	for (i = 0; a[i][7]; i++)
+	i = 0;
+	while (i < 8)
 	{
-		for (j = 0; j < 8; j++)
+		j = 0;
+		while (j < 8)
+		{
 			_putchar(a[i][j]);
-
-	_putchar('\n');
+			j++;
+		}
+		_putchar('\n');
+		i++;
 	}
 }
