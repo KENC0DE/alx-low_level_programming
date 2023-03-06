6aa5f7af3b2e400b398f6e24c435090c6b8f646a
KENC0DE <kenansameseret@gmail.com>
2023-03-06 21:53:45 +0530

    commiting

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _memset - fil the memo
+ * @s: -ponter
+ * @b: char
+ * @n: unsigned
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+why just tell me why man i don't understan
