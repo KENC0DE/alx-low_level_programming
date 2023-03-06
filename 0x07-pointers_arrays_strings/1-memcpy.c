0013d23cbc5f800820421c280a087bcb39d94a60
KENC0DE <kenansameseret@gmail.com>
2023-03-06 23:37:14 +0530

    commiting

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,7 @@
  * @n: ner
  * Return: char
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+/**char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
@@ -15,4 +15,15 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 		dest[i] = src[i];
 		i++;
 	}
+}*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
 }
