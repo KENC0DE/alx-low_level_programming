553b6e8f9f833ff448e3750e06ff796bd180342a
KENC0DE <kenansameseret@gmail.com>
2023-03-08 18:45:39 +0530

    committing

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
   * print_number - Prints any integer with putchar
@@ -11,11 +10,11 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n = -n;
 	}
 	if (n / 10)
 		print_number(n / 10);
 
-	putchar(n % 10 + '0');
+	_putchar(n % 10 + '0');
 }
