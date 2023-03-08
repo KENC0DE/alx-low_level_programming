f58d64c268df544f3be408d3c5766932952829bc
KENC0DE <kenansameseret@gmail.com>
2023-03-08 18:41:19 +0530

    committing

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,13 @@
   */
 void print_number(int n)
 {
-	unsigned int x;
-
 	if (n < 0)
 	{
-		_putchar('-');
-		n *= -1;
+		putchar('-');
+		n = -n;
 	}
+	if (n / 10)
+		print_number(n / 10);
 
-	x = n;
-
-	if (x / 10)
-		print_number(x / 10);
-
-	_putchar(x % 10 + '0');
+	putchar(n % 10 + '0');
 }
