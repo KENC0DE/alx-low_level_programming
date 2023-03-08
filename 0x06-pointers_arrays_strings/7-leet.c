5bcf21851f59435fbd17ff641aebbf45e3a18e85
KENC0DE <kenansameseret@gmail.com>
2023-03-08 12:15:21 +0530

    committing

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,32 +2,31 @@
 
 /**
   * leet - Encodes a string into 1337
-  * @s: The string to encode
+  * @str: The string to encode
   *
   * Return: The encoded string
   */
-char *leet(char *s)
+char *leet(char *str)
 {
-	int a = 0, b = 0, l = 5;
-	char r[5] = {'A', 'E', 'O', 'T', 'L'};
-	char n[5] = {'4', '3', '0', '7', '1'};
+	unsigned int i = 0;
+	unsigned int n = strlen(str);
 
-	while (s[a])
+	while (i < n)
 	{
-		b = 0;
+		if (str[i] == 'a' || str[i] == 'A')
+			str[i] = '4';
+		else if (str[i] == 'e' || str[i] == 'E')
+			str[i] = '3';
+		else if (str[i] == 'o' || str[i] == 'O')
+			str[i] = '0';
+		else if (str[i] == 't' || str[i] == 'T')
+			str[i] = '7';
+		else if (str[i] == 'l' || str[i] == 'L')
+			str[i] = '1';
+		else
+			str[i] = str[i];
 
-		while (b < l)
-		{
-			if (s[a] == r[b] || s[a] - 32 == r[b])
-			{
-				s[a] = n[b];
-			}
-
-			b++;
-		}
-
-		a++;
+		i++;
 	}
-
-	return (s);
+	return (str);
 }
