c4fa1789ca2af47d66431fe8cd100b7ebafcf20e
KENC0DE <kenansameseret@gmail.com>
2023-03-11 05:16:58 +0530

    commiting

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print number of agument passed
+ * @argc: args
+ * @argv: strargs
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", ((atoi(argv[1])) * (atoi(argv[2]))));
+	}
+}
