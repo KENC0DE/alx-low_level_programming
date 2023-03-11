8a30747967e53da77bbf2339fae7f1dac110bcd5
KENC0DE <kenansameseret@gmail.com>
2023-03-11 08:03:20 +0530

    commiting

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,71 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
 
 /**
- * coinConverter - Helper function that does all the mathematics
- * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
- */
-int coinConverter(int i)
-{
-	int count = 0;
-
-	while (i != 0)
-	{
-		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-			i -= 10;
-		else if (i % 5 == 0)
-			i -= 5;
-		else if (i % 2 == 0)
-		{
-			if (i % 10 == 6)
-				i -= 1;
-			else
-				i -= 2;
-		}
-		else
-			i -= 1;
-
-		count++;
-	}
-
-	return (count);
-}
-
-/**
- * main - Takes in exactly one argument for minimum coin count
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ *main - entry point
+ *
+ *Description: prints the minimum number of coins to make change for an amount
+ *of money
+ *
+ *@argc: amount of arguments passed through the program
+ *@argv: pointer that contains the arrays of the arguments
+ *
+ *Return: the minimum number of coins needed to make the change for the amount
+ *of money required
  */
 int main(int argc, char *argv[])
 {
-	int i, coin;
-
-	coin = 0;
+	int x = 0, money = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	i = atoi(argv[1]);
-
-	if (i < 0)
-		printf("0\n");
-	else
+	money = atoi(argv[1]);
+	while (money > 0)
 	{
-		coin = coinConverter(i);
-
-		printf("%d\n", coin);
+		if (money - 25 >= 0)
+		{
+			money = money - 25;
+		}
+		else if (money - 10 >= 0)
+		{
+			money = money - 10;
+		}
+		else if (money - 5 >= 0)
+		{
+			money = money - 5;
+		}
+		else if (money - 2 >= 0)
+		{
+			money = money - 2;
+		}
+		else if (money - 1 == 0)
+		{
+			money = money - 1;
+		}
+		x++;
 	}
-
+	printf("%d\n", x);
 	return (0);
 }
