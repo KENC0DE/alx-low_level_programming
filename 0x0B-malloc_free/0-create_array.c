7cdceb19b66519736f1783f6b76837bb0a846a22
KENC0DE <kenansameseret@gmail.com>
2023-03-14 23:21:45 +0530

    commiting

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,13 +19,12 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	ptr = malloc(sizeof(char) * size);
+	ptr = malloc((sizeof(char) * size) + 1);
 
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
 	}
-	ptr[size] = '\0';
 
 	return (ptr);
 }
