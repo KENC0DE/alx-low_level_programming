3004fa3ce0af8ca0f917aea3613f327a71d45230
KENC0DE <kenansameseret@gmail.com>
2023-03-17 01:00:38 +0530

    commiting

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,34 +1,45 @@
-#include "main.h"
-#include <string.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * string_nconcat - concat n strings
- * @s1: first
- * @s2: second
- * @n: times
- * Return: char
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ *
+ * Return: pointer to the resulting string
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
-	char *ptr = malloc(strlen(s1) + n + 1);
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (ptr == NULL)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
 		return (NULL);
 
-	if (n >= strlen(s2))
-		n = strlen(s2);
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
 
-	if (s1 == NULL)
-		s1 = "";
-	else if (s2 == NULL)
-		s2 = "";
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
 
-	strcpy(ptr, s1);
-	strncat(ptr + strlen(s1), s2, n);
-	ptr[strlen(s1) + n] = '\0';
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
 
-	return (ptr);
+	s[i] = '\0';
+
+	return (s);
 }
