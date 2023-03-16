c54fa6e3338e5077b57a624814c94d584aeb4883
KENC0DE <kenansameseret@gmail.com>
2023-03-17 00:26:28 +0530

    commiting

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * string_nconcat - concat n strings
+ * @s1: first
+ * @s2: second
+ * @n: times
+ * Return: char
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+	char strc[n];
+	char *ptr = malloc(strlen(s1) + (n + 1));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	if (s1 == NULL)
+		s1 = "";
+	else if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; i < n; i++)
+	{
+		strc[i] = s2[i];
+	}
+	strc[n] = '\0';
+
+	strcpy(ptr, s1);
+	strcat(ptr, strc);
+
+	return (ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,5 +2,6 @@
 #define KEN_HEADER
 
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif
