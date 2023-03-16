d9fb04419f2dfedcb3062b84c152f75cdfd66c38
KENC0DE <kenansameseret@gmail.com>
2023-03-17 02:10:24 +0530

    commiting

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,42 @@
-#include "main.h"
-#include <string.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * string_nconcat - concat n strings
- * @s1: first
- * @s2: second
- * @n: times
- * Return: char
- */
-
+* string_nconcat - prints concatenate string;
+* @s1: input string.
+* @s2: input string.
+* @n: len s2 string for print.
+* Return: Nothing.
+*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, len1 = 0, len2 = 0, j = 0;
-
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	char *ptr = NULL;
+	unsigned int l1, i, e;
+	char *a;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	if (n >= len2)
-		n = len2;
+	l1 = 0;
+	while (s1[l1])
+		l1++;
 
-	ptr = malloc(sizeof(*ptr) * len1 + n);
+	a = malloc(sizeof(*a) * l1 + n + 1);
 
-	if (ptr == NULL)
+	if (a == NULL)
 		return (NULL);
 
-	while (i < (len1 + n + 1))
+	for (i = 0, e = 0; i < (l1 + n); i++)
 	{
-		if (i >= len1)
+		if (i < l1)
 		{
-			ptr[i] = s2[j];
-			j++;
+			a[i] = s1[i];
 		}
 		else
 		{
-			ptr[i] = s1[i];
+			a[i] = s2[e++];
 		}
-		i++;
 	}
-	ptr[i] = '\0';
-	return (ptr);
+	a[i] = '\0';
+	return (a);
 }
