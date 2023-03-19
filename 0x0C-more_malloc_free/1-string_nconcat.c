f40f7223bf9c58d6831d14f1af73fdbae6c61b28
KENC0DE <kenansameseret@gmail.com>
2023-03-19 23:44:41 +0530

    committing

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	ptr = strcpy(ptr, s1);
 	ptr = strncat(ptr, s2, n);
-	ptr[l1 + n] = '\0';
+	ptr[l1 + n + 1] = '\0';
 
 	return (ptr);
 }
