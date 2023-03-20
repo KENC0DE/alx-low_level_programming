f9d8a33e85a8ceea8b4ed8e6517d4305ddca59fc
KENC0DE <kenansameseret@gmail.com>
2023-03-20 18:56:53 +0530

    committing

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - edit dog
+ * @d: pointer to dog
+ * @name: dog name
+ * @age: how old it is
+ * @owner: who has the dog
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
