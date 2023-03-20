39c3c51b4eda6617c1056f2f437dd99de85c007b
KENC0DE <kenansameseret@gmail.com>
2023-03-21 01:46:26 +0530

    committing

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees dog memory
+ * @d: address of the memory
+ * Return: nothing
+*/
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
