457de7feaf1d6e2498bf9b369aed1dcd74433046
KENC0DE <kenansameseret@gmail.com>
2023-03-21 01:30:47 +0530

    committing

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,91 +1,38 @@
-#include <stdlib.h>
 #include "dog.h"
-
+#include <stdlib.h>
+#include <string.h>
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
-	return (i);
-}
-
-/**
- * *_strcpy - copies the string pointed to by src
- * including the terminating null byte (\0)
- * to the buffer pointed to by dest
- * @dest: pointer to the buffer in which we copy the string
- * @src: string to be copied
- *
- * Return: the pointer to dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int len, i;
-
-	len = 0;
-
-	while (src[len] != '\0')
-	{
-		len++;
-	}
-
-	for (i = 0; i < len; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-
-	return (dest);
-}
-
-/**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: pointer to the new dog (Success), NULL otherwise
- */
+ * new_dog -create new dog
+ * @name: dog name
+ * @age: it's age
+ * @owner: who owns it
+ * Return: loc new dog
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	int len1, len2;
+	dog_t *ndg;
 
-	len1 = _strlen(name);
-	len2 = _strlen(owner);
-
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	ndg = malloc(sizeof(dog_t));
+	if (ndg == NULL)
 		return (NULL);
-
-	dog->name = malloc(sizeof(char) * (len1 + 1));
-	if (dog->name == NULL)
+	ndg->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (ndg->name == NULL)
 	{
-		free(dog);
+		free(ndg);
 		return (NULL);
 	}
-	dog->owner = malloc(sizeof(char) * (len2 + 1));
-	if (dog->owner == NULL)
+
+	ndg->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (ndg->owner == NULL)
 	{
-		free(dog);
-		free(dog->name);
+		free(ndg);
+		free(ndg->name);
 		return (NULL);
 	}
-	_strcpy(dog->name, name);
-	_strcpy(dog->owner, owner);
-	dog->age = age;
 
-	return (dog);
+	strcpy(ndg->name, name);
+	strcpy(ndg->owner, owner);
+	ndg->age = age;
+
+	return (ndg);
 }
