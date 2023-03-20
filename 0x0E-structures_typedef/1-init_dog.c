2c7bbcab78e76abec68b311085855ce1fdbb85e3
KENC0DE <kenansameseret@gmail.com>
2023-03-20 19:01:36 +0530

    committing

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include "main.h"
 
 /**
  * init_dog - edit dog
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,6 @@
+#ifndef KEN_HEADER
+#define KEN_HEADER
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
