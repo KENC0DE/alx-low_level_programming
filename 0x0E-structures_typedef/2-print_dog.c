ca5342a1e3c45118d01614b2f60a4c2458bd1e4c
KENC0DE <kenansameseret@gmail.com>
2023-03-20 23:48:21 +0530

    committing

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,17 +11,11 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		;
+		return;
 	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-	if (d->age == 0)
-		printf("Age: (nil)\n");
-	else
-		printf("Ange: %f\n", d->age);
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
