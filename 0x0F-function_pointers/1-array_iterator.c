cbadd25ce1c47c05a20c0451cb122db9dbadfdb3
KENC0DE <kenansameseret@gmail.com>
2023-03-22 20:26:42 +0530

    committing

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
-	if (array == NULL || action == NULL)
+	if (array == NULL || size == 0 || action == NULL)
 		return;
 	for (i = 0; i < size; i++)
 	{
