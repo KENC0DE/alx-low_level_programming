f98c9e5530c0fbec00645015a93f2460b78ef5e7
KENC0DE <kenansameseret@gmail.com>
2023-03-22 19:43:35 +0530

    committing

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "head.h"
+#include "function_pointers.h"
 #include <stdio.h>
 
 /**
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef KEN
-#define KEN
+#ifndef KEN_HEADER
+#define KEN_HEADER
 
 int _putchar (char);
 void print_name(char *name, void (*f)(char *));
