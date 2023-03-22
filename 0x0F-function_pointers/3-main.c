e8ef5874f763bce801ff79570ebe0780978b50f7
KENC0DE <kenansameseret@gmail.com>
2023-03-23 02:29:23 +0530

    committing

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,3 @@
-#include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
@@ -9,7 +8,7 @@
  *
  * Return: Always 0.
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
