821bbe644b2d011c5d866003066a4def820959ba
KENC0DE <kenansameseret@gmail.com>
2023-03-23 22:41:01 +0530

    committing

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separetor: between the items
+ * @n: number of agurments passed
+ * Return: nothing
+*/
+void print_numbers(const char *separetor, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	if (separetor == NULL)
+		return;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		printf("%s", separetor);
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,5 +2,6 @@
 #define KEN_HEADER
 
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separetor, const unsigned int n, ...);
 
 #endif
