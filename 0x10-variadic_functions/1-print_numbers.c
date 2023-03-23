2ebc27da2bf5214becaaf51a2e9e89c0a9669b57
KENC0DE <kenansameseret@gmail.com>
2023-03-23 23:29:57 +0530

    committing

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,16 +13,12 @@ void print_numbers(const char *separetor, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	if (separetor == NULL)
-		return;
-
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if (i == n - 1)
-			break;
-		printf("%s", separetor);
+		if (i != n - 1 && separetor != NULL)
+			printf("%s", separetor);
 	}
 	printf("\n");
 	va_end(ap);
