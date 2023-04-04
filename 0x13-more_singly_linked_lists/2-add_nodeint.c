5f663aa36ec85be9e0b6f48fadccea0815d4cd76
KENC0DE <kenansameseret@gmail.com>
2023-04-04 23:40:42 +0530

    committing

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - ands new nodes at the beginning of the list
+ * @head: pointer to the first node
+ * @n: value to be added
+ *
+ * Return: number of conted nodes
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
