8b438b853eade22aa7485785f0530787866276d1
KENC0DE <kenansameseret@gmail.com>
2023-04-05 04:35:05 +0530

    committing

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sums all the datas in the linked lists
+ * @head: the target data
+ * Return: the sum of the data
+*/
+int sum_listint(listint_t *head)
+{
+	listint_t *ptr;
+	int sum = 0;
+
+	ptr = head;
+	if (ptr == NULL)
+		return (0);
+	
+	while (ptr)
+	{
+		sum += ptr->n;
+		ptr = ptr->next;
+	}
+
+	return (sum);
+}
