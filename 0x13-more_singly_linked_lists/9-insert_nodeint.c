b552a7e0e2adc86209f69299418eafd7598ccf5d
KENC0DE <kenansameseret@gmail.com>
2023-04-05 05:30:54 +0530

    committing

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,22 +11,30 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *back;
 	listint_t *front;
+	listint_t *count;
 	listint_t *new_node;
-	unsigned int i;
+	unsigned int i, j;
 
+	j = 0;
+	count = *head;
+	while (count)
+	{
+		count = count->next;
+		j++;
+	}
+	if (idx > j)
+		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
-
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-
 	i = 0;
 	back = *head;
 	while (i < (idx - 1))
@@ -37,6 +45,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	front = back->next;
 	back->next = new_node;
 	new_node->next = front;
-
 	return (new_node);
 }
