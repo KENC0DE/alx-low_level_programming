701d1abe68fe660ad01af7d4af22f4160eb75b99
KENC0DE <kenansameseret@gmail.com>
2023-04-11 00:39:01 +0530

    committing

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read text file print to standard out.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *tem;
+	ssize_t fd, w, t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	tem = malloc(sizeof(char) * letters);
+	t = read(fd, tem, letters);
+	w = write(STDOUT_FILENO, tem, t);
+
+	free(tem);
+	close(fd);
+	return (w);
+}
