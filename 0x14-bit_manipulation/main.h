34f7ea3c97391397e64a07287cf56e7377ebd298
KENC0DE <kenansameseret@gmail.com>
2023-05-04 15:28:33 +0530

    committing

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef KEN_HEADER
+#define KEN_HEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+unsigned int binary_to_uint(const char *b);
+int square(int a, int b);
+
+#endif
