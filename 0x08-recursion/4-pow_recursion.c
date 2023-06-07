199d2e42a92d7b7fa8d3aab62b50ddbdf693c3d5
KENC0DE <kenansameseret@gamil.com>
2023-06-08 00:37:20 +0530

    committing

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
+/**
+ * _pow_recursion - raise using recursion
+ * @x: number to be raised
+ * @y: raising constant
+ * Return: the raised number.
+*/
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return(x * _pow_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
