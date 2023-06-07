3cea751046f85ec2c68b0e8f0a204a839546377e
KENC0DE <kenansameseret@gamil.com>
2023-06-07 23:26:51 +0530

    committing

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reverse prints stirng
+ * @s: passed string to be reversed
+ * Return: nothing
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0' || *(s + 1) == '\0')
+	{
+		putchar(*s);
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
