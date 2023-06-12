eff8ceccf92c9283f538cbea0fd017207efd3f89
KENC0DE <kenansameseret@gamil.com>
2023-06-12 23:56:49 +0530

    committing

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenate passed argumets
+ * @ac: number of arguments passed
+ * @av: the arguments.
+ * Return: pointer to concatenated string.
+*/
+char *argstostr(int ac, char **av)
+{
+	char *conCated = NULL;
+	int idx, sidx, totLeng, pars, asgn, totP;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	totLeng = 0;
+	for (idx = 1; idx < ac; idx++)
+	{
+		for (sidx = 0; av[idx][sidx]; sidx++)
+			totLeng++;
+	}
+
+	conCated = malloc(sizeof(char) * (totLeng + ac));
+	if (conCated == NULL)
+		return (NULL);
+
+	totP = 0;
+	for (pars = 1; pars < ac; pars++)
+	{
+		for (asgn = 0; av[pars][asgn]; asgn++, totP++)
+		{
+			conCated[totP] = av[pars][asgn];
+		}
+		conCated[totP] = '\n';
+		totP++;
+	}
+	conCated[totLeng + ac] = '\0';
+
+	return (conCated);
+}
