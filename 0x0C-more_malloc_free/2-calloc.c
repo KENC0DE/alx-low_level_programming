d2c6ca0a211ff1062900b090d4de97325878a321
KENC0DE <kenansameseret@gamil.com>
2023-06-15 23:52:59 +0530

    committing

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,6 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *array = NULL;
-	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -18,8 +17,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
-		array[i] = 0;
-
+	memset(array, 0, size * nmemb);
 	return (array);
 }
