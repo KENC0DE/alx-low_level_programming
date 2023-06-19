dbfc5d69131d4d3b2946e810f9c398563ff60341
KENC0DE <kenansameseret@gamil.com>
2023-06-19 23:26:55 +0530

    committing

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * new_dog - create new dog int the structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: pointer to the new clreated dog info.
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *kenDog;
+
+	kenDog = malloc(sizeof(dog_t));
+	if (kenDog == NULL)
+		return (NULL);
+	kenDog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (kenDog->name == NULL)
+		return (NULL);
+	kenDog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (kenDog->owner == NULL)
+		return (NULL);
+
+	strcpy(kenDog->name, name);
+	strcpy(kenDog->owner, owner);
+	kenDog->age = age;
+
+	return (kenDog);
+}
+
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,8 @@
 #define KEN_HEADER
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dog - dog list.
@@ -19,6 +21,7 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
 
