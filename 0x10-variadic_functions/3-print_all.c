f4bc332602197ed297181a031e0665c4e81783e0
KENC0DE <kenansameseret@gamil.com>
2023-06-22 20:48:55 +0530

    committing

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints any argument passed to the function.
+ * @format: string type pointed.
+ * Return: nothing.
+*/
+void print_all(const char * const format, ...)
+{
+	int k, idx;
+	va_list suck;
+	char *str;
+
+	idx = 0;
+	while (format[idx])
+	{
+		idx++;
+	}
+
+	k = 0;
+	va_start(suck, format);
+	while (k < idx)
+	{
+		switch (format[k])
+		{
+			case 'c':
+				printf("%c ", (char)va_arg(suck, int));
+				break;
+			case 'i':
+				printf("%d ", va_arg(suck, int));
+				break;
+			case 'f':
+				printf("%f ", va_arg(suck, double));
+				break;
+			case 's':
+				str = va_arg(suck, char *);
+				if (str == NULL)
+					printf("(nil) ");
+				else
+					printf("%s ", str);
+			default:
+				break;
+		}
+		k++;
+	}
+	printf("\n");
+}
+
