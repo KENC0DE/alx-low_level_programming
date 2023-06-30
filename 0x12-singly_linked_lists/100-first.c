c626dbdbf94afe9431d446e565d93e20e940fc7d
KENC0DE <kenansameseret@gamil.com>
2023-07-01 01:58:06 +0530

    committed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,17 +7,7 @@ void executeFirst(void) __attribute__((constructor));
 */
 void executeFirst(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
