fb0d580a0046bfea9912da4e9298316da0dfc144
KENC0DE <kenansameseret@gamil.com>
2023-07-01 02:15:16 +0530

    committed

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,22 @@
 section .data
-    msg db 'Hello, Holberton', 0
+    hello db "Hello, Holberton", 10  ; 10 is the ASCII value for newline
+    len equ $ - hello                ; Length of the string
 
 section .text
     global _start
 
 _start:
-    ; Write the string to standard output
-    mov eax, 4            ; System call number for write
-    mov ebx, 1            ; File descriptor for stdout
-    mov ecx, msg          ; Address of the string
-    mov edx, 3            ; Length of the string
-    int 0x80              ; Call the kernel
+    ; Prepare the arguments for the write system call
+    mov eax, 1            ; System call number for write
+    mov edi, 1            ; File descriptor for stdout
+    mov esi, hello        ; Address of the string
+    mov edx, len          ; Length of the string
+
+    ; Call the write system call
+    syscall
 
     ; Exit the program
-    mov eax, 1            ; System call number for exit
-    xor ebx, ebx          ; Exit status code 0
-    int 0x80              ; Call the kernel
+    mov eax, 60           ; System call number for exit
+    xor edi, edi          ; Exit status code 0
+    syscall
 
