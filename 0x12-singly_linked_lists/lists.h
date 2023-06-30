46b507f32dd994c119cb52dde240bcdb310088dd
KENC0DE <kenansameseret@gamil.com>
2023-06-30 21:08:01 +0530

    committed

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef MY_LIST
-#define MY_LIST
+#ifndef MY_LISTS
+#define MY_LISTS
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,9 +15,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
