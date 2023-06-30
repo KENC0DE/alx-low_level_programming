6d8fd33e308e166a6d02bb5e35d912c947e41e55
KENC0DE <kenansameseret@gamil.com>
2023-07-01 02:26:50 +0530

    committed

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,9 @@
-section .data
-    message db 'Hello, Holberton', 0
-
-section .text
-    global _start
-
-_start:
-    ; Write the message to stdout
-    mov eax, 4               ; Syscall number for write
-    mov ebx, 1               ; File descriptor (stdout)
-    mov ecx, message         ; Message to be printed
-    mov edx, 3               ; Length of the message
-    int 0x80                 ; Call the kernel
-
-    ; Exit the program
-    mov eax, 1               ; Syscall number for exit
-    xor ebx, ebx             ; Exit code 0
-    int 0x80                 ; Call the kernel
-
+          global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
