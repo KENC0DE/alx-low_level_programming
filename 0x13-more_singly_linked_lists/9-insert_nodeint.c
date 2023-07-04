2d887b6393ddabf269a767cec71d1857df675af2
KENC0DE <kenansameseret@gamil.com>
2023-07-04 21:38:00 +0530

    committed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,41 +1,41 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts new node at the a given index.
- * @head: pointer to the first node of the list.
- * @idx: index where the new node gets added
- * @n: value to be stored in the new node.
- * Return: sum of the linked list data.
-*/
+ * insert_nodeint_at_index - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is
+ * added.
+ * @n: integer element.
+ *
+ * Return: the address of the new node, or NULL if it
+ * failed.
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *tp;
-	listint_t *new;
 	unsigned int i;
-	int check = (int)idx;
+	listint_t *new;
+	listint_t *h;
 
-	if (head == NULL)
-		return (NULL);
-	if (*head == NULL || check < 0)
-		return (NULL);
+	h = *head;
 
-	tp = *head;
-
-	if (idx > 0)
+	if (idx != 0)
 	{
-		for (i = 0; i < idx - 1 && tp; i++)
+		for (i = 0; i < idx - 1 && h != NULL; i++)
 		{
-			tp = tp->next;
+			h = h->next;
 		}
-		if (tp == NULL)
-			return (NULL);
 	}
+
+	if (h == NULL && idx != 0)
+		return (NULL);
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
+
 	if (idx == 0)
 	{
 		new->next = *head;
@@ -43,9 +43,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else
 	{
-		new->next = tp->next;
-		tp->next = new;
+		new->next = h->next;
+		h->next = new;
 	}
+
 	return (new);
 }
 
