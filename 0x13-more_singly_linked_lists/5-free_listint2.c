d5ab8434d9382c02df1958ba3cbd7d538e091b2a
KENC0DE <kenansameseret@gamil.com>
2023-07-04 19:06:40 +0530

    committed

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free every a given node.
+ * @head: pointer to the first node of the list.
+ * Return: Nothing.
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *tp;
+
+	if (*head == NULL)
+		return;
+
+	while (*head)
+	{
+		tp = (*head)->next;
+		free(*head);
+		*head = tp;
+	}
+}
+
