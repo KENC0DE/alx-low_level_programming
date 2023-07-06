c70d6deac5d691b80a187434e503cd722722dab3
KENC0DE <kenansameseret@gamil.com>
2023-07-06 21:09:55 +0530

    committed

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary form of the number given.
+ * @n: number passed to the function.
+ * Return: nothing.
+*/
+void print_binary(unsigned long int n)
+{
+	int bit;
+
+	if (n == 0 || n == 1)
+	{
+		printf("%lu", n);
+		return;
+	}
+	bit = n & 1;
+	print_binary(n >> 1);
+	printf("%d", bit);
+}
+
