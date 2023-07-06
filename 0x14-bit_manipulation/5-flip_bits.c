c3212f0e5f2075c8f1e55e1420772f9d1b3c6c28
KENC0DE <kenansameseret@gamil.com>
2023-07-07 00:20:00 +0530

    committed

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - calculate the number of bit needed to be flipped.
+ * @n: the first number.
+ * @m: the second number.
+ * Return: return number of flips.
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int bit;
+	unsigned int flips;
+
+	bit = n ^ m;
+	for (flips = 0; bit; bit = bit >> 1)
+	{
+		if (bit & 1)
+			flips++;
+	}
+
+	return (flips);
+}
+
