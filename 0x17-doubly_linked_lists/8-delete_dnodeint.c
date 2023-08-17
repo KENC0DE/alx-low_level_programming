c0449f061fea136a32f9af8ae99bef3a754c1243
KENC0DE <kenansameseret@gamil.com>
2023-08-17 14:30:13 +0530

    back to C

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - del node at idx.
+ * @head: head of the list.
+ * @index: idex to be del.
+ *
+ * Return: val del -1 or 1
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *tp;
+
+	if (!head || !(*head))
+		return (-1);
+
+	tp = *head;
+	if (index == 0)
+	{
+		if (tp->next)
+		{
+			*head = tp->next;
+			(*head)->prev = NULL;
+			free(tp);
+			return (1);
+		}
+		else
+		{
+			free(tp);
+			*head = NULL;
+			return (1);
+		}
+	}
+	while (i < index)
+	{
+		if (i < index && !tp)
+			return (-1);
+		if (i + 1 == index && tp->next == NULL)
+		{
+			tp->prev->next = NULL;
+			free(tp);
+			return (1);
+		}
+		tp = tp->next;
+		i++;
+	}
+	tp->prev->next = tp->next;
+	free(tp);
+	return (1);
+}
+
