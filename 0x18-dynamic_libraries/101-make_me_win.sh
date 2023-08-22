8a18761e6691dcaf3d5a62666fca410b4f5fed81
KENC0DE <kenansameseret@gamil.com>
2023-08-22 06:48:44 +0530

    overwrite

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/KENC0DE/alx-low_level_programming/blob/master/0x18-dynamic_libraries/100-operations.so
-export LD_RELOAD=../100-operations.so
+wget -P ../ https://github.com/KENC0DE/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libfake.so
+export LD_RELOAD=../libfake.so
