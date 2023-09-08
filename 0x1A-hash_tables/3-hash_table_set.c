b87b9d751c6b8db1a9b3ed38a943585ce96ec885
KENC0DE <kenansameseret@gmail.com>
2023-09-08 21:53:53 +0530

    hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key || !value)
 		return (0);
 
-	idx = key_index((unsigned char *)key, ht->size);
+	idx = key_index((const unsigned char *)key, ht->size);
 	val = strdup(value);
 	ky = strdup(key);
 
@@ -25,17 +25,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!nw)
 		return (0);
 
+	if (ht->array[idx] && strcmp(key, ht->array[idx]->key) == 0)
+	{
+		ht->array[idx]->value = val;
+		return (1);
+	}
+
 	nw->key = ky;
 	nw->value = val;
 	nw->next = NULL;
 
-	if (ht->array[idx] && strcmp(key, ht->array[idx]->key) == 0)
-	{
-		nw->next = ht->array[idx];
-		ht->array[idx] = nw;
-		return (1);
-	}
-
+	nw->next = ht->array[idx];
 	ht->array[idx] = nw;
 
 	return (1);
