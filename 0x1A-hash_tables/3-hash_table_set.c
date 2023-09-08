680064f3e3fd5bc2b7a8636b9b1dfd0e3a9b857c
KENC0DE <kenansameseret@gmail.com>
2023-09-08 21:25:53 +0530

    hash

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - sets hash value in the hash table
+ * @ht: the table
+ * @key: the key to be set.
+ * @value: value for the key.
+ *
+ * Return: 1 on success and 0 on failure.
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int idx;
+	char *val, *ky;
+	hash_node_t *nw;
+
+	if (!ht || !key || !value)
+		return (0);
+
+	idx = key_index((unsigned char *)key, ht->size);
+	val = strdup(value);
+	ky = strdup(key);
+
+	nw = malloc(sizeof(hash_node_t));
+	if (!nw)
+		return (0);
+
+	nw->key = ky;
+	nw->value = val;
+	nw->next = NULL;
+
+	if (ht->array[idx] && strcmp(key, ht->array[idx]->key) == 0)
+	{
+		nw->next = ht->array[idx];
+		ht->array[idx] = nw;
+		return (1);
+	}
+
+	ht->array[idx] = nw;
+
+	return (1);
+}
+
