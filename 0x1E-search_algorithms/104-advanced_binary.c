97e44dc8ea34dbc9af53a581a4d2cc53ddc7458e
KENC0DE <kenansameseret@gmail.com>
2023-12-14 01:54:37 +0530

    search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,14 +1,14 @@
 #include "search_algos.h"
 
 /**
- * print - prints the passed array of integers.
+ * printAd - prints the passed array of integers.
  * @array: array to be printed.
  * @start: point from where printing starts.
  * @end: where printing ends.
  *
  * Return: nothing.
 */
-void print(int *array, size_t start, size_t end)
+void printAd(int *array, size_t start, size_t end)
 {
 	printf("Searching in array: ");
 	for (; start < end; start++)
@@ -18,48 +18,55 @@ void print(int *array, size_t start, size_t end)
 }
 
 /**
- * advanced_binary - search for a value in a sorted array using binary search.
+ * binary_recur - recursion helper.
+ * @array: the search array.
+ * @value: target value.
+ * @left: left index.
+ * @right: right index.
  *
- * @array: pointer to the first element of the array to be searched.
- * @size: size of the array to be searched.
- * @value: value to be searched in the array.
+ * Return: index of value.
+*/
+int binary_recur(int *array, int value, size_t left, size_t right)
+{
+	size_t mid = (left + right) / 2;
+
+	printAd(array, left, right);
+
+	if (value == array[mid])
+	{
+		while (1)
+		{
+			if (array[mid - 1] != value)
+				break;
+
+			mid--;
+		}
+		return (mid);
+	}
+	if (mid == 0 || left == right)
+		return (-1);
+
+	if (value > array[mid])
+		left = mid + 1;
+	else
+		right = mid - 1;
+
+	return (binary_recur(array, value, left, right));
+}
+
+/**
+ * advanced_binary - optimized binary serach
  *
- * Return: the index of the value.
+ * @array: pointer to the first integer of the list.
+ * @size: the size of the list.
+ * @value: value to be searched in the list.
+ *
+ * Return: the index where the value is found.
 */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t left, mid, right, cur;
-
-	if (array == NULL)
+	if (!array)
 		return (-1);
 
-	left = 0;
-	right = size - 1;
-
-	while (left <= right)
-	{
-		print(array, left, right);
-
-		mid = (left + right) / 2;
-
-		if (value == array[mid])
-		{
-			cur = mid;
-			while (1)
-			{
-				if (array[cur - 1] != value)
-					break;
-
-				cur--;
-			}
-			return (cur);
-		}
-
-		if (value > array[mid])
-			left = mid + 1;
-		else
-			right = mid - 1;
-	}
-
-	return (-1);
+	return (binary_recur(array, value, 0, size - 1));
 }
